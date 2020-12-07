c572aefd84ce3435f44d2b1cd86681486b6f453f
Fabian Kastner <kastner.fabian@gmx.at>
2020-12-08 00:53:31 +0100

    wip: trained model, now to testing

--- modules/machine_learning.py
+++ modules/machine_learning.py
@@ -8,7 +8,11 @@
 
 import os
 import pickle
+import logging
 import datetime
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+logging.getLogger('tensorflow').setLevel(logging.FATAL)
 
 import numpy as np
 import pandas as pd
@@ -17,9 +21,26 @@ from sklearn.preprocessing import MinMaxScaler
 from modules.retrieve_data import get_df_from_symbol
 from modules.visualize_data import plot_data
 
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM
+from tensorflow.keras.models import load_model
+
 # -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-   
+
+# change tensorflow log level
+def set_tf_loglevel(level):
+    if level >= logging.FATAL:
+        os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+    if level >= logging.ERROR:
+        os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+    if level >= logging.WARNING:
+        os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
+    else:
+        os.environ['TF_CPP_MIN_LOG_LEVEL'] = '0'
+    logging.getLogger('tensorflow').setLevel(level)
+
+
 def test_1():
     data = pickle.load(open("data/PLTR--2020-12-06.pickle", "rb"))
 
@@ -74,6 +95,7 @@ def test_1():
     print("MSE error for standard averaging: {0}".format(round(0.5 * np.mean(mse_errors), 5)))
 
     import matplotlib.pyplot as plt
+    plt.style.use("fivethirtyeight")
     plt.figure(figsize = (18,9))
     plt.plot(range(25, train_data.shape[0] + 25), train_data, color='blue', label='True')
     plt.plot(range(window_size, N), std_avg_predictions, color='orange', label='Prediction')
@@ -88,14 +110,18 @@ def test_1():
 class DataGenerator:
 
     # constructor
-    def __init__(self, data, batch_size, num_unroll):
+    def __init__(self, data, num_unroll, batch_size=None):
         self._prices = data[["high", "low", "open", "close"]]
         self._prices_length = self._prices.shape[0] - num_unroll
         
-        self._volume = data["volume"]
+        # self._volume = data["volume"]
         
-        self._batch_size = batch_size
         self._num_unroll = num_unroll
+
+        if not batch_size:
+            self._batch_size = self._prices_length - self._num_unroll
+        else:
+            self._batch_size = batch_size if batch_size <= self._prices_length - self._num_unroll else self._prices_length - self._num_unroll
         
         self._cursor = self._num_unroll
         
@@ -118,20 +144,42 @@ class DataGenerator:
     # get next batch
     def next_batch(self):
         if self._cursor >= self._prices_length: self._empty = True
-        batches = []
+        batch_X = []
+        batch_y = []
         
         for i in range(self._batch_size):
             X = self._prices.iloc[self._cursor - self._num_unroll:self._cursor, :].values
             y = self._prices.iloc[self._cursor, :].values
             
-            batches.append([X, y])
+            batch_X.append(X)
+            batch_y.append(y)
             
             self._cursor += 1
-        return batches
+        
+        return np.asarray(batch_X), np.asarray(batch_y)
+
+    # scale data to [0, 1]
+    def scale_data(self):
+        scaler = MinMaxScaler()
+        self._prices = pd.DataFrame(scaler.fit_transform(self._prices), columns = self._prices.columns)
+        # self._volume = pd.DataFrame(scaler.fit_transform(self._volume), columns = self._volume.columns)
 
     # check if empty
     def empty(self):
         return self._empty
+
+
+def get_model(input_shape):
+    model = Sequential()
+
+    model.add(LSTM(50, return_sequences=True, input_shape=input_shape))
+    model.add(LSTM(50, return_sequences=False))
+    model.add(Dense(25))
+    model.add(Dense(1))
+
+    model.compile(optimizer="adam", loss="mean_squared_error")
+
+    return model
 
 
 def test_2():
@@ -144,16 +192,31 @@ def test_2():
 
     data = pickle.load(open("data/PLTR--2020-12-06.pickle", "rb"))
 
-    dg = DataGenerator(data, batch_size=4, num_unroll=3)
+    dg = DataGenerator(data, num_unroll=10)
+    dg.scale_data()
 
-    while not dg.empty():
-        batch = dg.next_batch()
-        print(batch[0])
-        print(batch[1])
-        print(batch[2])
+    batch_X = None
+    batch_y = None
 
-    print(data.shape)
+    batch_X, batch_y = dg.next_batch()
     
+    # print(batch_X.shape)
+    # print(batch_y.shape)
+
+    model_name = "{0}--{1}.model".format(symbol, datetime.datetime.now().strftime("%Y-%m-%d"))
+    # create model
+    model = get_model(input_shape=batch_X.shape[1:])
+
+    # train model
+    set_tf_loglevel(logging.INFO)
+    model.fit(batch_X, batch_y, batch_size=32, epochs=3)
+
+    # save model
+    pickle.dump(model, open("models/{0}".format(model_name), "wb"))
+
+    # load model
+    # model = load_model("models/{0}".format(model_name))
+
 
 # -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
