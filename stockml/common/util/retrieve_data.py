dd7ca8a16a497a476060c708a89ec2c3ee6c30ee
Fabian Kastner <kastner.fabian@gmx.at>
2020-12-27 18:58:05 +0100

    adjusted data retrieval

--- stockml/common/util/retrieve_data.py
+++ stockml/common/util/retrieve_data.py
@@ -8,6 +8,9 @@ import http.client
 import configparser
 import sqlite3
 import datetime
+import time
+import sys
+import os
 
 from tqdm import tqdm
 import pandas as pd
@@ -15,6 +18,15 @@ from alpha_vantage.timeseries import TimeSeries
 
 # -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def spinning_cursor():
+    while True:
+        for cursor in "⠁⠂⠄⡀⢀⠠⠐⠈":
+            yield cursor
+
+
+def console_log(message):
+    print("[{0}] [{1}]".format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), message))
 
 
 def get_df_from_symbol(symbol, key, interval):
@@ -50,39 +62,56 @@ def load_data():
     interval = "1min"
 
     conn = sqlite3.connect('stock_data.db')
-
     cursor = conn.cursor()
 
+    limit = 100
+    batch_from = 0
 
-    # cursor.execute('''CREATE TABLE one_min (symbol text, date text, open real, high real, low real, close real, volume int)''')
-    # conn.commit()
-    # cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-    # print(cursor.fetchall())
-    # cursor.execute("SELECT * FROM one_min")
-    # print(cursor.fetchall())
-
-
-    limit = 3
-
-    for index, row in tqdm(stock_list_df.iterrows()):
+    for index, row in stock_list_df.iterrows():
         if index >= limit: break
+
+        success = False
 
         symbol = row["Symbol"]
 
         latest_date_datetime = pd.to_datetime("1970-01-01 00:00:00", format="%Y-%m-%d %H:%M:%S")
 
-        latest_date_df = pd.read_sql_query("SELECT date FROM one_min WHERE symbol == '{0}' ORDER BY date DESC LIMIT 1".format(symbol), conn);
+        latest_date_df = pd.read_sql_query("SELECT * FROM one_min WHERE symbol = '{0}' ORDER BY date DESC LIMIT 1".format(symbol), conn);
            
         if not latest_date_df.empty:
             latest_date_datetime = pd.to_datetime(latest_date_df['date'][0], format="%Y-%m-%d %H:%M:%S")
         
-        if (datetime.datetime.now() - latest_date_datetime).days >= 3:
-            data = get_df_from_symbol(symbol, key, interval)
-            data = data[data['date'] > latest_date_datetime]
-            data["symbol"] = symbol
-            data.to_sql("one_min", conn, if_exists="replace")
+        days_since_last_update = (datetime.datetime.now() - latest_date_datetime).days
 
-    conn.commit()
+        if days_since_last_update >= 5:
+
+            print("Updating {}".format(symbol))
+            while not success:
+                try:
+                    data = get_df_from_symbol(symbol, key, interval)
+
+                    data = data[data['date'] > latest_date_datetime]
+                    data["symbol"] = symbol
+                    data["date"] = data["date"].dt.strftime("%Y-%m-%d %H:%M:%S")
+
+                    data_list = list(data.itertuples(index=False, name=None))
+
+                    cursor.executemany("INSERT INTO one_min (date, open, high, low, close, volume, symbol) VALUES (?, ?, ?, ?, ?, ?, ?)", data_list)
+                    conn.commit()
+                    success = True
+
+                # reached api call frequency limit
+                except ValueError as e:
+                    console_log("({0}-{1}/{2}) up-to-date".format(str(batch_from + 1), str(index), str(limit)))
+                    batch_from = index
+
+                    spinner = spinning_cursor()
+                    for _ in range(600):
+                        sys.stdout.write(next(spinner))
+                        sys.stdout.flush()
+                        time.sleep(0.1)
+                        sys.stdout.write('\b')
+
     conn.close()
 
 
